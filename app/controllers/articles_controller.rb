5a4aeaa5fb75a7994f57d89093a9d935072cb973
Kashif Ali <kashifali874@gmail.com>
2017-05-05 14:17:40 -0700

    edit and delete working for every user

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -65,6 +65,9 @@ class ArticlesController < ApplicationController
   # DELETE /articles/1
   # DELETE /articles/1.json
   def destroy
+    if current_user != @article.user
+      redirect_to "/articles"
+    end
     @article.destroy
     respond_to do |format|
       format.html { redirect_to articles_url, notice: 'Article was successfully destroyed.' }
