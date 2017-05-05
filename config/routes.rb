0aca8c665d5c244804f7b0a30fc7fb0356ea1e75
Kashif Ali <kashifali874@gmail.com>
2017-05-05 15:22:09 -0700

    minor fix

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
-  resources :comments
-  resources :articles
+
+  resources :articles do
+    resources :comments
+  end
 
   root "articles#home"
   get '/profile', to: "user#profile"
