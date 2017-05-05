9bfda5dc5f9546c2cc7417d0ad0f0a21dffa755d
Kashif Ali <kashifali874@gmail.com>
2017-05-05 16:36:35 -0700

    Comments box added on articel page

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,7 +1,8 @@
 class CommentsController < ApplicationController
-  before_action :set_comment, only: [:show, :edit, :update, :destroy]
-    before_action :authenticate_user!, except: [:index, :show]
-  before_action :current_user, only: [:edit, :update, :destroy]
+  # before_action :set_comment
+  # before_action :set_comment, only: [:show, :edit, :update, :destroy]
+  # before_action :authenticate_user!, except: [:index, :show]
+  # before_action :current_user, only: [:edit, :update, :destroy]
 
   # GET /comments
   # GET /comments.json
@@ -11,9 +12,9 @@ class CommentsController < ApplicationController
 
   # GET /comments/1
   # GET /comments/1.json
-  def show
+  # def show
 
-  end
+  # end
 
   # GET /comments/new
   def new
@@ -21,17 +22,17 @@ class CommentsController < ApplicationController
   end
 
   # GET /comments/1/edit
-  def edit
-    if current_user != @comment.user
-      redirect_to root_path
-  end
+  # def edit
+  #   if current_user != @comment.user
+  #     redirect_to root_path
+  # end
 
   # POST /comments
   # POST /comments.json
   def create
     @comment = current_user.comments.build(comment_params)
-
-    respond_to do |format|
+      # redirect_to @article
+    # respond_to do |format|
       if @comment.save
         format.html { redirect_to @comment, notice: 'Comment was successfully created.' }
         format.json { render :show, status: :created, location: @comment }
@@ -44,33 +45,37 @@ class CommentsController < ApplicationController
 
   # PATCH/PUT /comments/1
   # PATCH/PUT /comments/1.json
-  def update
-    respond_to do |format|
-      if @comment.update(comment_params)
-        format.html { redirect_to @comment, notice: 'Comment was successfully updated.' }
-        format.json { render :show, status: :ok, location: @comment }
-      else
-        format.html { render :edit }
-        format.json { render json: @comment.errors, status: :unprocessable_entity }
-      end
-    end
-  end
+  # def update
+  #   respond_to do |format|
+  #     if @comment.update(comment_params)
+  #       format.html { redirect_to @comment, notice: 'Comment was successfully updated.' }
+  #       format.json { render :show, status: :ok, location: @comment }
+  #     else
+  #       format.html { render :edit }
+  #       format.json { render json: @comment.errors, status: :unprocessable_entity }
+  #     end
+  #   end
+  # end
 
   # DELETE /comments/1
   # DELETE /comments/1.json
-  def destroy
-    @comment.destroy
-    respond_to do |format|
-      format.html { redirect_to comments_url, notice: 'Comment was successfully destroyed.' }
-      format.json { head :no_content }
-    end
-  end
+  # def destroy
+  #   @comment.destroy
+  #   respond_to do |format|
+  #     format.html { redirect_to comments_url, notice: 'Comment was successfully destroyed.' }
+  #     format.json { head :no_content }
+  #   end
+  # end
 
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_comment
       @comment = Comment.find(params[:id])
     end
+
+    # def set_article
+    #   @Article = Article.find(params[:article_id])
+    # end
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def comment_params
