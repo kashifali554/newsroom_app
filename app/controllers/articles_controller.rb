0c7b4e705b5c959572fd1fd1733c6ad996c2ff49
Kashif Ali <kashifali874@gmail.com>
2017-05-10 12:39:42 -0700

    Skinny abilities

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -123,7 +123,7 @@ class ArticlesController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def article_params
-      params.require(:article).permit(:headline, :img, :text, :tags, :category, :published)
+      params.require(:article).permit(:headline, :img, :text, :category)
     end
 
 end
--- app/models/ability.rb
+++ app/models/ability.rb
@@ -5,38 +5,22 @@ class Ability
 
     if user && user.role == 2
       can :manage, :all
+    end
 
-    elsif user && user.role == 1
-
-      can :new, Article do |article|
+    if user && user.role == 1
+      alias_action :create, :read, :update, :destroy, :to => :crud
+      can :crud, Article do |article|
         article.user == user
       end
+    end
 
-      can :create, Article do |article|
-          article.user == user
-      end
-
-      can :edit, Article do |article|
-          article.user == user
-      end
-
-      can :update, Article do |article|
-          article.user == user
-      end
-
-      can :Destroy, Article do |article|
-          article.user == user
-      end
-
-
-    else user && user.role == nil
+    if user && user.role == nil
       can :read, :all
-
       can :create, Comment do |comment|
         comment.user = user
       end
+    end
 
-    end #end of if statment
   end # end of initializer method
 end #end of class
 
