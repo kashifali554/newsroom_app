78a5c3b8ed58e4e92b4bea98cdfc325deb8b93af
Kashif Ali <kashifali874@gmail.com>
2017-05-15 19:02:41 -0700

    seed data and model validations

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,5 +3,11 @@
 #
 # Examples:
 #
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+#   movies = Movie.create({ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Article.destroy_all
+#Add as much articles seed following this pattern
+Article.create({id: 1, headline:"Adorable Kitty", img:"http://i.imgur.com/UtxDmzY.jpg", text:"This is the post text", category:"latest", user_id:6})
+
+p "Article count:", Article.count
